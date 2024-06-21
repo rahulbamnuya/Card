6ca53675ee700e7e63bd868147d94e6a88833adb
rahulbamnuya <129646171+rahulbamnuya@users.noreply.github.com>
2024-06-21 23:59:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-// let x=document.getElementById("main");
-// // x.innerHTML="<h1>Goodbye</h1><h1>Goodbye</h1><h1>Goodbye</h1><h1>Goodbye</h1><h1>Goodbye</h1>"
-// let y="";
-// for(let i=1;i<=4;i++){
-// y+="<h1>Goodbye </h1>";
-// }
-// x.innerHTML=y;
-// x.innerHTML=y;
-{/* <div class="card">
-    <img src="/DOM/Card/doraemon.png" alt="">
-</div>
-<div class="card">
-    <img src="/DOM/Card/hathodi.png" alt="">
-</div>
-<div class="card">
-    <img src="/DOM/Card/ranger.png" alt="">
-</div> */}
-let char=["./image/pikachu.png","./image/hathodi.png","./image/ranger.png","./image/doraemon.png"]
+
+let char=["pikachu.png","hathodi.png","ranger.png","doraemon.png"]
 var main=document.getElementById("main");
 var s=' ';
 for(let i=0;i<55;i++){
